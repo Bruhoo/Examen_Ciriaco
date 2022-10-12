acf20c0f7c406378acd7e6e91de984a1cd4e2beb
Jose Fernando <josefernandoc01@gmail.com>
2022-10-12 09:39:36 -0500

    Pregunta01 - Listo por Cachay

---
+++ Preg01.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int A,B,x,m;
+	
+	cout<<"Digite un valor A: "; cin>>A;
+	cout<<"Digite un valor B: "; cin>>B;
+	
+	x=A;
+	m=B;
+	
+	
+	cout<<"\nDatos intercanbiados: "<<"\nA: "<<m<<"\nB: "<<x;
+	
+	
+	
+	return 0;
+}
